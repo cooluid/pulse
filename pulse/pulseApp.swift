cee35554f7681bc19b60700bebe5590915d3b10a
cooluid <400822@163.com>
2026-08-10 14:58:45 +0800

    first commit

---
+++ pulse/pulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulseApp.swift
+//  pulse
+//
+//  Created by fanr on 2026/8/10.
+//
+
+import SwiftUI
+
+@main
+struct pulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
